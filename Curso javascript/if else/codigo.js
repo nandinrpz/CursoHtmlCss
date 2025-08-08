7671f1815fa007885fedba5c1cc7a1c21d530458
Fernando Girão <f.girao2015@outlook.com>
2025-08-08 10:41:27 -0300

    Move if/else example to new directory structure
    Relocated the if/else JavaScript example from 'if/else' to 'if else' directory, adding sample code and HTML. Removed the old empty files from the previous directory.

---
+++ Curso javascript/if else/codigo.js
@@ -0,0 +1,22 @@
+/*if(condicao) {
+
+}else {
+    
+}
+
+if (condicao) {
+    // Código a ser executado se a condição for verdadeira
+}else if (outraCondicao) {
+    // Código a ser executado se a outra condição for verdadeira
+} else {
+    // Código a ser executado se nenhuma das condições anteriores for verdadeira
+}*/
+
+var idade = 26
+if (idade <= 18) {
+    document.white("Você é menor de idade.")
+}else if (idade > 18 && idade <= 60) {
+    document.white("Você é adulto.")
+}else {
+    document.white("Você é idoso.")
+}
