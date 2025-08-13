fea184ee49f59e16b3e475958400e7ce4d9440aa
Fernando Girão <f.girao2015@outlook.com>
2025-08-12 21:24:57 -0300

    Add casting example and update if-else logic
    Added new casting example with HTML and JS files to demonstrate parsing user input and summing values. Updated 'if else/codigo.js' to comment out the age check and add a new grade evaluation logic using prompt and parseFloat.

---
+++ Curso javascript/casting/casting.js
@@ -0,0 +1,7 @@
+var variavel1 = prompt("Digite o primeiro valor: ")
+var variavel2 = prompt("Digite o segundo valor: ")
+
+variavel1 = parseInt(variavel1)
+variavel2 = parseInt(variavel2)
+
+document.write("A soma dos valores é: " + (variavel1 + variavel2) + "<br>")
--- Curso javascript/if else/codigo.js
+++ Curso javascript/if else/codigo.js
@@ -12,11 +12,22 @@ if (condicao) {
     // Código a ser executado se nenhuma das condições anteriores for verdadeira
 }*/
 
-var idade = 26
+/*var idade = prompt("Digite sua idade: ")
+idade = parseInt(idade)
 if (idade <= 18) {
     document.white("Você é menor de idade.")
 }else if (idade > 18 && idade <= 60) {
     document.white("Você é adulto.")
 }else {
     document.white("Você é idoso.")
+}*/
+
+var nota = prompt("Digite a nota do aluno: ")
+nota = parseFloat(nota)
+var media = 7
+
+if (nota >= media) {
+    document.write("Aprovado")
+}else {
+    document.write("Reprovado")
 }
