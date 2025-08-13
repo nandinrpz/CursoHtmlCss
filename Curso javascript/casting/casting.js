ef7ebcaacdd38400bbeb541edddf6971acee1b95
Fernando Girão <f.girao2015@outlook.com>
2025-08-12 22:30:22 -0300

    Add logical operators practice and update casting example
    Added new files for practicing if-else and logical operators, including an HTML file and a JavaScript file with example conditions. Updated casting.js to use fixed values and demonstrate string conversion instead of user input and parsing.

--- Curso javascript/casting/casting.js
+++ Curso javascript/casting/casting.js
@@ -1,7 +1,8 @@
-var variavel1 = prompt("Digite o primeiro valor: ")
-var variavel2 = prompt("Digite o segundo valor: ")
+var variavel1 = 10
+var variavel2 = 20
 
-variavel1 = parseInt(variavel1)
-variavel2 = parseInt(variavel2)
 
-document.write("A soma dos valores é: " + (variavel1 + variavel2) + "<br>")
+//variavel1 = parseInt(variavel1)
+//variavel2 = parseFloat(variavel2)
+
+document.write(variavel1.toString() + variavel2.toString(), "<br>")
---
+++ Curso javascript/praticando if-else e operadores logicos/operadores.js
@@ -0,0 +1,21 @@
+// todas as condições abaixo devem ser verdadeiras para que o resultado seja verdadeiro
+// true && true = true
+if(2 == 2 && 3 >= 1) {
+    document.write("Verdadeiro");
+}else {
+    document.write("Falso");
+}
+// apenas uma condição true é suficiente para que o resultado seja verdadeiro
+//true || false = true 
+if(2 == 2 || 3 >= 1) {
+    document.write("Verdadeiro");
+}else {
+    document.write("Falso");
+}
+// inverte o valor booleano
+// true = false
+if(!4 == 4) {
+    document.write("Verdadeiro");
+}else {
+    document.write("Falso");
+}
