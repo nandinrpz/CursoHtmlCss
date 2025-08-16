5b5cb8d0a6db0145eb26daba83f9bfeb88c4df6d
Fernando Girão <f.girao2015@outlook.com>
2025-08-15 21:42:19 -0300

    Add switch/case example for number input in JS
    Introduces a simple JavaScript example using switch and case statements to handle user input. Includes an HTML file that loads the script and prompts the user for a number, displaying a message based on the input.

---
+++ Curso javascript/switch and case/codigo.js
@@ -0,0 +1,17 @@
+var parametro = prompt("Digite um número");
+
+switch (parseInt(parametro)) {
+    case 1:
+        document.write("parametro 1");
+        break;
+    case 2:
+        document.write("parametro 2");
+        break;
+    case 3:
+        document.write("parametro 3");
+        break;
+    default:
+        document.write("Número não encontrado");
+        
+
+}
