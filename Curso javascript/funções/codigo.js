c02767770a11dee6925a515b9789d0e0b5765bf1
Fernando Girão <f.girao2015@outlook.com>
2025-08-25 19:09:25 -0300

    Add area calculation function and HTML page
    Introduces a JavaScript function to calculate the area of a terrain and displays the result. Adds an HTML file that loads and executes the script.

---
+++ Curso javascript/funções/codigo.js
@@ -0,0 +1,8 @@
+function calcularAreaTerreno(largura, comprimento){
+
+    //lógica da função
+    var area = largura * comprimento;
+    document.write("A área do terreno é: " + area + "m²");
+}
+
+calcularAreaTerreno(15, 25);
